89cd9267237a2e656e7e45b73c4cc9adca8420ad
Jezebel1990 <jezebelguedes@gmail.com>
2023-03-30 01:50:23 -0300

    add board

---
+++ src/components/Board/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Hero from '../Hero';
+
+const Board  = () => {
+    return (
+
+        <div>
+            <Hero/>
+        <img src="./assets/tileset.gif" alt="" />
+        </div>
+    );
+}
+
+export default Board;
---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Hero = () => {
+return (
+        <img src="./assets/HERO.png" alt="Hero" />
+    )
+}
+export default Hero;
