2bc4d53e273572088be7bcbeedbad3e72d1e9dad
Jezebel1990 <jezebelguedes@gmail.com>
2023-03-30 16:32:12 -0300

    Creating the starting position of the game

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { TILE_SiZE } from '../settings/constants';
+import './index.css';
 
 const Hero = () => {
 return (
-        <img src="./assets/HERO.png" alt="Hero" />
-    )
+    <div
+    style={{
+      position:'absolute',
+      bottom:0,
+      left:0,
+      width: TILE_SiZE,
+      height: 150,
+      backgroundImage:"url(./assets/HERO.png)" ,
+      backgroundRepeat: 'no-repeat',
+      animation: "hero-animation 10s steps(6) infinite"
+    }}
+    />
+    ) ;
 }
 export default Hero;
---
+++ src/components/settings/constants.ts
@@ -0,0 +1,2 @@
+export const TILE_SiZE = 92;
+export const GAME_SIZE = 960;
